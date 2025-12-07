070568d24a8c14012b91a3a2f5d7120968c8c4a5
kiki <74992761+kiki2010@users.noreply.github.com>
2025-12-07 17:07:23 -0300

    now we have the nearest station, I need to look for meteorological data now
    Let's go!

--- cbamultiriskv1/lib/services/wudata.dart
+++ cbamultiriskv1/lib/services/wudata.dart
@@ -3,9 +3,11 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:math';
 
-const apiKey = '026cda1f35b54cddacda1f35b53cdda3'; //This is a secret lol
+const apiKey = 'Nya'; //This is a secret lol
 
 class WeatherStationService {
+
+  String? _selectedStationId;
   Map<String, dynamic>? _stationSaved;
 
   Future<Map<String, dynamic>> getNearestStation(Position position) async {
@@ -19,10 +21,42 @@ class WeatherStationService {
 
     if (response.statusCode != 200) {
       throw Exception('error al obtener datos api');
+    } 
+    else if (response.statusCode == 200) {  
+      final data = json.decode(response.body);
+
+      final stationsIds = data['location']['stationId'];
+      final updateTimes = data['location']['updateTimeUtc'];
+      final distances = data['location']['distanceKm'];
+
+      final List<Map<String, dynamic>> stations = [];
+
+      //Select the avaiable statations
+      for (int i = 0; i < stationsIds.length; i++) {
+        if (updateTimes[i] != null) {
+          stations.add({
+            'stationId': stationsIds[i],
+            'updateTime': updateTimes[i],
+            'distance': distances[i],
+          });
+        }
+      }
+
+      stations.sort((a, b) {
+        int updateComparison = b['updateTime'].compareTo(a['updateTime']);
+        if (updateComparison != 0) return updateComparison;
+        return a['distance'].compareTo(b['distance']);
+      });
+
+      //Save the first station || selected station
+      if (stations.isNotEmpty) {
+        _selectedStationId = stations.first['stationId'];
+      }
+      else {
+        throw Exception("No valid station");
+      }
+      _stationSaved = data;
     }
-    
-    final data = json.decode(response.body) as Map<String, dynamic>;
-    _stationSaved = data;
-    return data;
+    return _stationSaved!;
   }
 }
