eebd6acf6ebf254da9bd2ee8c27ec043f2304d43
kiki <74992761+kiki2010@users.noreply.github.com>
2025-12-07 12:37:06 -0300

    added permission and internet services
    Yupiii | I wan't able to test it yet

--- cbamultiriskv1/lib/main.dart
+++ cbamultiriskv1/lib/main.dart
@@ -6,7 +6,6 @@ import 'package:cbamultiriskv1/services/firepredict.dart';
 import 'package:cbamultiriskv1/services/floodpredict.dart';
 import 'package:cbamultiriskv1/services/getlocation.dart';
 import 'package:cbamultiriskv1/services/wudata.dart';
-
 import 'package:geolocator/geolocator.dart';
 
 void main() async {
@@ -26,33 +25,10 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('MultiRisk'),
         ),
-        body: const Center(
+        body: Center(
           child: Text('Test'),
         ),
       ),
     );
   }
-}
-
-Future<Position> getUserLocation() async {
-  bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
-  if (!serviceEnabled) {
-    throw Exception('Is not posible to get location.');
-  }
-
-  LocationPermission permission = await Geolocator.checkPermission();
-  if (permission == LocationPermission.denied) {
-    permission = await Geolocator.requestPermission();
-    if (permission == LocationPermission.denied) {
-      throw Exception('Location permission denied.');
-    }
-  }
-
-  if (permission == LocationPermission.deniedForever) {
-    throw Exception('Location permission denied forever.');
-  }
-
-  return await Geolocator.getCurrentPosition(
-    desiredAccuracy: LocationAccuracy.high,
-  );
 }
