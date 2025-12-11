46bfe19885a5dff28a4e7115be580658d0a761bc
kiki <74992761+kiki2010@users.noreply.github.com>
2025-12-11 19:16:46 -0300

    now the IA model works correctly

--- cbamultiriskv1/lib/services/floodpredict.dart
+++ cbamultiriskv1/lib/services/floodpredict.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'wudata.dart';
 
 import 'package:geolocator/geolocator.dart';
@@ -11,14 +12,14 @@ class FloodPrediction {
 
   Future<void> loadFloodModel() async {
     try {
-      _interpreter = Interpreter.fromAddress('assets/flood_model.tflite' as int);
+      _interpreter = await Interpreter.fromAsset('assets/flood_model.tflite');
       print('model loaded correctly');
     } catch (e) {
       print('error $e');
     }
   }
 
-  Future<void> predictFlood(Position position) async {
+  Future<String> predictFlood(Position position) async {
     final weatherService = WeatherStationService();
 
     final allData = await weatherService.getAllWeatherData(position);
@@ -26,5 +27,31 @@ class FloodPrediction {
     final historicalData = allData['historical'];
 
     //We create variables for all the data we will use for getting the risk
+    double spi = historicalData['spi'] ?? 0.0;
+    double precipTotal = actualData['rain'] ?? 0.0;
+    double precipRate = actualData['precipRate'] ?? 0.0;
+    double humidity = (actualData['humidity'] ?? 0.0) / 100;
+
+    List<double> floodInputData = [spi, precipTotal, precipRate, humidity];
+
+    final floodInput = [Float32List.fromList(floodInputData)];
+    final floodOutput = List.generate(1, (_) => List.filled(3, 0.0));
+
+    _interpreter?.run(floodInput, floodOutput);
+
+    final prediction = floodOutput[0];
+    final maxValue = prediction.reduce((a, b) => a > b ? a : b);
+    final FloodPredictedClass = prediction.indexOf(maxValue);
+
+    switch (FloodPredictedClass) {
+      case 0:
+        return "LOW";
+      case 1:
+        return 'MEDIUM';
+      case 2:
+        return 'HIGH';
+      default:
+        return 'Unknown';
+    } 
   }
 }
