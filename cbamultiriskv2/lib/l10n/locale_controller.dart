137aa4d42982bd050b88fe1f115f599511a5f74d
kiki <74992761+kiki2010@users.noreply.github.com>
2026-01-07 10:50:21 -0300

    Now the locale is saved and the theme color too!
    Let's go!

--- cbamultiriskv2/lib/l10n/locale_controller.dart
+++ cbamultiriskv2/lib/l10n/locale_controller.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LocaleController extends ChangeNotifier {
-  Locale _locale = const Locale('en');
+  static const _key = 'locale';
 
+  Locale _locale = const Locale('es');
   Locale get locale => _locale;
+
+  LocaleController() {
+    _loadLocale();
+  }
 
   void setLocale(Locale locale) {
     _locale = locale;
+    _saveLocale();
     notifyListeners();
   }
 
-  bool get isEnglish => _locale.languageCode == 'en';
+  Future<void> _loadLocale() async {
+    final prefs = await SharedPreferences.getInstance();
+    final code = prefs.getString(_key) ?? 'es';
+    _locale = Locale(code);
+    notifyListeners();
+  }
+
+  Future<void> _saveLocale() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_key, _locale.languageCode);
+  }
 }
