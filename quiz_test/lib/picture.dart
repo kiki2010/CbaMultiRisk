3787e384b0c29039585275867fc83fc39d96787f
kiki <74992761+kiki2010@users.noreply.github.com>
2026-01-13 14:15:24 -0300

    no funciona ahora, pero despues seguro lo hago funcionar

---
+++ quiz_test/lib/picture.dart
@@ -0,0 +1,11 @@
+// here we use the image 
+// which we use in our app
+
+// this is the image 
+const String base = "assets/";  
+
+// this is assets image you 
+// can download according to you
+const String balloon = "${base}0.png"; 
+const String ideas = "${base}0.png";
+const String balloon2 = "${base}0.png";
